ffd27b2d94c5a7c766002f944ea46bd739b1b608
loren <loren@LAPTOP-9NR5200T>
2020-05-12 10:58:04 +0200

    JavaDoc

--- EmpleadoJavaDoc/src/EmpleadoJavaDoc/Empleado.java
+++ EmpleadoJavaDoc/src/EmpleadoJavaDoc/Empleado.java
@@ -1,20 +1,47 @@
 package EmpleadoJavaDoc;
 
+/**
+ * 
+ * @author lorena
+ * @version 1_2020
+ * @since 12/05/2020
+ *
+ */
+
 public class Empleado {
-	
+
 	private String nombre;
 	private String apellido;
 	private double salario;
 
+	/**
+	 * 
+	 * @param nombre   -- Nombre del empleado
+	 * @param apellido -- Primer apellido del empleado
+	 * @param salario  -- Salario base del empleado
+	 */
 	public Empleado(String nombre, String apellido, double salario) {
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.salario = salario;
 	}
 
+	// Métodos
+
+	/**
+	 * 
+	 * @see -- Empleado (See--> Determina la clase a la que hace referencia ese metodo)
+	 * @param subida -- cantidad que se sube 
+	 */
+
 	public void subirsalario(double subida) {
 		salario = salario + subida;
 	}
+	
+	/**
+	 * 
+	 * @return "true" si nombre no está vacio 
+	 */
 
 	private boolean comprobar() {
 		if (nombre.equals("")) {
